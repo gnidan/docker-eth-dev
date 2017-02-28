1b7d514c242fbcc55f097aef5e124186f7584757
Chris Hatch <hatch@tuta.io>
2017-02-28 10:03:35 +0700

    #6 restore ropsten in example dapp truffle.js

--- dapps/example/truffle.js
+++ dapps/example/truffle.js
@@ -4,6 +4,10 @@ module.exports = {
       host: "testrpc",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    "ropsten": {
+      host: "parity-ropsten",
+      port: 8545
     }
   }
 };
